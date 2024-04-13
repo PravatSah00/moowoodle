d132c7cb8c51e96231a12d6c657ea30d50123134
Bishal221 <kayalbishal221@gmail.com>
2024-04-13 18:34:33 +0530

    Some others fix
    Some others fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c8a78ca5663b1581bf3e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '5c9171afb3158679a844');
